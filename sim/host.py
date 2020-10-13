cd4e25cea9e307041b30fc5dfe6fa9f5813817de
ccie18643 <ccie18643@gmail.com>
2020-10-13 01:04:45 +0000

    Redesigned so it can be used to create new header as well

---
+++ sim/host.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+
+PyTCP, Python TCP/IP stack simulation version 0.1 - 2020, Sebastian Majewski
+host.py - contains class responsible for emulating host
+
+"""
+
+import asyncio
+
+
+class Host:
+    """ Class responsible for emulating host """
+
+    def __init__():
+        """ Class constructor """
+        pass
+
+    def receiver(packet):
+        """ Inbound packet receiver """
+        pass
+
+    async def sender():
+        """ Coroutine responsible for sending out packets """
+
+        packet = self.packet_queue.pop(0)
+
+        self.routing_table[packet.dst_addr](packet)
+
+        await asyncio.sleep(1)
