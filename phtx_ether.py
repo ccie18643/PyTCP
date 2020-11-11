abf9be6e46cd602172b17977cd11e928831d8209
ccie18643 <ccie18643@gmail.com>
2020-11-11 16:51:42 +0000

    Added support for routing packets to external networks over default gateway

--- phtx_ether.py
+++ phtx_ether.py
@@ -15,6 +15,12 @@ import stack
 def phtx_ether(self, child_packet, ether_src="00:00:00:00:00:00", ether_dst="00:00:00:00:00:00"):
     """ Handle outbound Ethernet packets """
 
+    def __atoi(ip_address):
+        from struct import unpack
+        from socket import inet_aton
+
+        return unpack("!L", inet_aton(ip_address))[0]
+
     def __send_out_packet():
         self.logger.opt(depth=1).debug(f"{ether_packet_tx.tracker} - {ether_packet_tx}")
         stack.tx_ring.enqueue(ether_packet_tx, urgent=True if child_packet.protocol == "ARP" else False)
@@ -36,16 +42,36 @@ def phtx_ether(self, child_packet, ether_src="00:00:00:00:00:00", ether_dst="00:
     if ether_packet_tx.ether_type == ps_ether.ETHER_TYPE_IP:
         ip_packet_tx = ps_ip.IpPacket(ether_packet_tx)
 
-        # Send out packet if its destinied to one of our broadcast addresses
-        if ip_packet_tx.ip_dst in self.stack_ip_broadcast:
+        # Send out packet if its destinied to limited broadcast addresses
+        if ip_packet_tx.ip_dst in "255.255.255.255":
             ether_packet_tx.ether_dst = "ff:ff:ff:ff:ff:ff"
             self.logger.debug(f"{ether_packet_tx.tracker} - Resolved destiantion IP {ip_packet_tx.ip_dst} to MAC {ether_packet_tx.ether_dst}")
             __send_out_packet()
             return
 
+        # Send out packet if its destinied to directed broadcast or netowork addresses (in relation to its source IP)
+        for stack_ip_address in self.stack_ip_address:
+            if stack_ip_address[0] == ip_packet_tx.ip_src:
+                if ip_packet_tx.ip_dst in {stack_ip_address[2], stack_ip_address[3]}:
+                    ether_packet_tx.ether_dst = "ff:ff:ff:ff:ff:ff"
+                    self.logger.debug(f"{ether_packet_tx.tracker} - Resolved destiantion IP {ip_packet_tx.ip_dst} to MAC {ether_packet_tx.ether_dst}")
+                    __send_out_packet()
+                    return
+
+        # Send out packet if is destined to external network (in relation to its source IP) and we are able to obtain MAC of default gateway from ARP cache
+        for stack_ip_address in self.stack_ip_address:
+            if stack_ip_address[0] == ip_packet_tx.ip_src:
+                if not __atoi(stack_ip_address[2]) <= __atoi(ip_packet_tx.ip_dst) <= __atoi(stack_ip_address[3]):
+                    if mac_address := stack.arp_cache.find_entry(stack_ip_address[4]):
+                        ether_packet_tx.ether_dst = mac_address
+                        self.logger.debug(
+                            f"{ether_packet_tx.tracker} - Resolved destiantion IP {ip_packet_tx.ip_dst} to Default Gateway MAC {ether_packet_tx.ether_dst}"
+                        )
+                        __send_out_packet()
+                        return
+
         # Send out packet if we are able to obtain destinaton MAC from ARP cache
-        mac_address = stack.arp_cache.find_entry(ip_packet_tx.ip_dst)
-        if mac_address:
+        if mac_address := stack.arp_cache.find_entry(ip_packet_tx.ip_dst):
             ether_packet_tx.ether_dst = mac_address
             self.logger.debug(f"{ether_packet_tx.tracker} - Resolved destiantion IP {ip_packet_tx.ip_dst} to MAC {ether_packet_tx.ether_dst}")
             __send_out_packet()
