5f667384fbc4fdc43f442d045132a43ee61b875e
ccie18643 <ccie18643@gmail.com>
2020-11-27 03:21:52 +0000

    ipv6_eui64 method takes prefix in IPv6Network format now

--- ipv6_helper.py
+++ ipv6_helper.py
@@ -8,18 +8,17 @@ ipv6_helper.py - module contains IPv6 helper functions
 """
 
 from re import sub
-from ipaddress import IPv6Interface, IPv6Address
+from ipaddress import IPv6Interface, IPv6Network, IPv6Address
 
 
-def ipv6_eui64(mac, prefix="fe80::/64"):
+def ipv6_eui64(mac, prefix=IPv6Network("fe80::/64")):
     """ Create IPv6 EUI64 address """
 
-    prefix_network, prefix_mask = prefix.split("/")
     eui64 = sub(r"[.:-]", "", mac).lower()
     eui64 = eui64[0:6] + "fffe" + eui64[6:]
     eui64 = hex(int(eui64[0:2], 16) ^ 2)[2:].zfill(2) + eui64[2:]
     eui64 = ":".join(eui64[_ : _ + 4] for _ in range(0, 16, 4))
-    return IPv6Interface(prefix_network + eui64 + "/" + prefix_mask)
+    return IPv6Interface(str(prefix.network_address) + eui64 + "/" + str(prefix.prefixlen))
 
 
 def ipv6_solicited_node_multicast(ipv6_address):
