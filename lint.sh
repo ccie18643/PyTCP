007f91287b2d2329cf999f06261d7146da738b7c
ccie18643 <ccie18643@gmail.com>
2021-01-03 16:20:12 +0000

    Code cleanup

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-codespell -w --ignore-words-list="ect,ether,nd,tha" --quiet-level=2 *py
-pyupgrade --py36-plus *py
-isort --profile black *.py
-black -l 160 *.py
-flake8 *.py
+PY_PATH=$(find . -name '*.py')
+codespell -w --ignore-words-list="ect,ether,nd,tha" --quiet-level=2 ${PY_PATH} README.md
+isort --profile black ${PY_PATH}
+black -l 160 ${PY_PATH}
+flake8 ${PY_PATH}
