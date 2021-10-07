902e115d82d454292160d4542b56b87e31c4100a
ccie18643 <ccie18643@gmail.com>
2021-10-06 21:10:53 -0500

    Working on UDP packet flow testing

--- tests/udp_phtx.py
+++ tests/udp_phtx.py
@@ -33,6 +33,7 @@ from __future__ import annotations  # Required by Python ver < 3.10
 
 from testslide import TestCase
 
+from pytcp.misc.tx_status import TxStatus
 from pytcp.misc.packet_stats import PacketStatsTx
 from tests.mock_network import (
     MockNetworkSettings,
@@ -52,3 +53,30 @@ class TestUdpPhtx(TestCase):
         setup_mock_packet_handler(self)
 
     # Test name format: 'test_name__test_description__optional_condition'
+
+    def test_udp_phtx__ip4_udp_packet__no_data(self):
+        """Test sending IPv4/UDP packet"""
+
+        tx_status = self.packet_handler._phtx_udp(
+            ip_src=self.mns.stack_ip4_host.address,
+            ip_dst=self.mns.host_a_ip4_address,
+            udp_sport=1000,
+            udp_dport=2000,
+        )
+        self.assertEqual(tx_status, TxStatus.PASSED__ETHER__TO_TX_RING)
+        '''
+        self.assertEqual(
+            self.packet_handler.packet_stats_tx,
+            PacketStatsTx(
+                ip4__pre_assemble=1,
+                ip4__mtu_ok__send=1,
+                ether__pre_assemble=1,
+                ether__src_unspec__fill=1,
+                ether__dst_unspec__ip4_lookup=1,
+                ether__dst_unspec__ip4_lookup__locnet__arp_cache_hit__send=1,
+            ),
+        )
+        with open(TEST_FRAME_DIR + "ip4_to_unicast_address_on_local_network__src_valid.tx", "rb") as _:
+            frame_tx = _.read()
+        self.assertEqual(self.frame_tx[: len(frame_tx)], frame_tx)
+        '''
